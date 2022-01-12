a2b4f9b7ff6b6f916bec77458a488655fb07229e
Dan Groleau <ddgroleau@gmail.com>
2022-01-12 11:42:00 -0700

    Updated notify.js

--- functions/notify.js
+++ functions/notify.js
@@ -2,7 +2,7 @@
 exports = async function(changeEvent) {
     const axios = require('axios').default;
     if(changeEvent.fullDocument) {
-        const {contactName,contactEmail,contactCompany,contactMessage} = changeEvent.fullDocument;
+        const {contactName,contactEmail,contactMessage,contactCompany} = changeEvent.fullDocument;
         const response = await axios.post('https://ddgroleau-api.herokuapp.com/notification',{
                         contactName:contactName,
                         contactEmail:contactEmail,
