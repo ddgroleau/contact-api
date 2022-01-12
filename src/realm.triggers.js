041c62eb47f8aaa27780d87fce147eef4921c968
Dan Groleau <ddgroleau@gmail.com>
2022-01-12 11:05:57 -0700

    Updated realm trigger.

--- src/realm.triggers.js
+++ src/realm.triggers.js
@@ -1,20 +1,21 @@
 // This is the body of the MongoDB Atlas Realm Trigger Function. 
 // The function is configured to invoke on new contact insert.
 
-export const newContact = (changeEvent) => {
-    const newContact = changeEvent.fullDocument;
-    fetch('https://ddgroleau-api.herokuapp.com/notification', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    contactName:newContact.contactName,
-                    contactEmail:newContact.contactEmail,
-                    contactCompany:newContact.contactCompany,
-                    contactMesssage:newContact.contactMesssage,
-                })
-            }).then(response => {
-                console.log(response.json());
-            }).catch(response => console.log(response))
-}
+exports = async function(changeEvent) {
+    if(changeEvent.fullDocument) {
+        const newContact = changeEvent.fullDocument;
+        const response = await fetch('https://ddgroleau-api.herokuapp.com/notification', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        contactName:newContact.contactName,
+                        contactEmail:newContact.contactEmail,
+                        contactCompany:newContact.contactCompany,
+                        contactMesssage:newContact.contactMesssage,
+                    })
+                }).catch(response => console.log(response));
+        console.log(response.json());
+    };
+};
