cead241721ebada8f3cfe978a37bc27204d6ec70
Dan Groleau <ddgroleau@gmail.com>
2022-01-22 11:04:21 -0700

    Updated trigger function.

--- mongodb_realm/functions/notify.js
+++ mongodb_realm/functions/notify.js
@@ -3,6 +3,7 @@ exports = async function(changeEvent) {
     const axios = require('axios').default;
     if(changeEvent.fullDocument) {
         const {contactName,contactEmail,contactCompany,contactMessage} = changeEvent.fullDocument;
+        if(!contactEmail) return new Error('Email was invalid.');
         const response = await axios.post('https://ddgroleau-api.herokuapp.com/notification',{
                         contactName:contactName,
                         contactEmail:contactEmail,
