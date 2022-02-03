2aceed193f4a180e5ad7c9f66011b6056e48078a
Dan Groleau <ddgroleau@gmail.com>
2022-02-02 20:24:56 -0700

    Added CORS support.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    cors: true
   });
   await app.listen((process.env.PORT || 3000));
 }
